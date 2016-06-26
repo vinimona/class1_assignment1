23cc5610dd9988110c57f7124e750db6daac580f
vinimona <90vinimona23@gmail.com>
2016-06-26 16:42:25 +0530

    Add files via upload

---
+++ class1_assignment1.java
@@ -0,0 +1,14 @@
+package Class1_assignments;
+
+public class class1_assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int val1 = 100;
+		float val2 = 200.25F;
+		
+		int sum = val1 + (int)val2;
+		System.out.println("Sum = "+ sum);
+	}
+}
